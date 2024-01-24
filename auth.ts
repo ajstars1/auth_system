796e7cb58b184dd3eb789f8ae12dde9f833e6724
AJ Stars <53529358+ajstars1@users.noreply.github.com>
2024-01-24 17:29:44 +0530

    add: middleware setup added

---
+++ auth.ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+import { db } from "./lib/db";
+import authConfig from "@/auth.config";
+
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+    adapter: PrismaAdapter(db),
+    session: { strategy: "jwt" },
+    ...authConfig,
+});
---
+++ routes.ts
@@ -0,0 +1,35 @@
+/**
+ * An array of routes that are accessible to the public.
+ * These routes are not protected by authentication.
+ * @type {string[]}
+ */
+
+export const publicRoutes = [
+    "/"
+];
+
+/**
+ * An array of routes that are used for authentication.
+ * These routes will redirect logged in users to /settings.
+ * @type {string[]}
+ */
+
+export const authRoutes = [
+    "/auth/login",
+    "/auth/register",
+];
+
+
+/**
+ * The prefix for API authentication routes.
+ * Routes that start with this prefix are used for API authentication processes.
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth";
+
+
+/**
+ * The default redirect path after logging In.
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
