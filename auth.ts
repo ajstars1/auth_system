a0cb95b8224051e6a4d1ba4c163455a2b8cb128e
AJ Stars <53529358+ajstars1@users.noreply.github.com>
2024-01-25 16:10:59 +0530

    feat: added callbacks

--- auth.ts
+++ auth.ts
@@ -1,16 +1,64 @@
-import NextAuth from "next-auth";
+import NextAuth, { type DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { JWT } from "next-auth/jwt";
 
+import { getUserById } from "@/data/user";
 import { db } from "./lib/db";
 import authConfig from "@/auth.config";
+import { UserRole } from "@prisma/client";
+
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      role: UserRole,
+    } & DefaultSession["user"]
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+      role?: UserRole,
+    } 
+}
+
 
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
-  signOut
+  signOut,
 } = NextAuth({
-    adapter: PrismaAdapter(db),
-    session: { strategy: "jwt" },
-    ...authConfig,
+
+
+
+
+  callbacks: {
+    async session({ session, token }) {
+      // console.log({session, token});
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+
+
+
+
+    async jwt({ token }) {
+      if (!token?.sub) return token;
+        const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+      return token; 
+    },
+  },
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
 });
